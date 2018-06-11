bd6c56f2b01a91a0024d9e3c8bc4fd913a239884
wushunhao <875686769@qq.com>
2018-06-11 08:47:15 +0800

    first

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.hiSlider').hiSlider({
+        isFlexible: true,
+        isShowTitle: false,
+        isShowControls: false,
+        intervalTime: 3000,
+    });
+});
